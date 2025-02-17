7835a884abc158cd6b876b5c6fd5ead1a05e45b1
enrbaewebdaniel <enrbaewebdaniel@gmail.com>
2025-02-17 16:13:08 +0100

    feat: remove Hero component and associated styles for a cleaner structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
