5e1a7b84994b568f3aede53940c8aa2007f73dbf
enrbaewebdaniel <enrbaewebdaniel@gmail.com>
2025-02-28 17:31:00 +0100

    feat: add Services component and update layout; enhance styles and spacing for improved presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '@app/menu/menu.component';
 import { ExperienceComponent } from '@app/experience/experience.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, ExperienceComponent],
+  imports: [MenuComponent, ExperienceComponent, ServicesComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ src/app/services/services.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  imports: [],
+  templateUrl: './services.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ServicesComponent {}
