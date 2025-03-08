83edab8997fb67837536cd8e14255548e234c5b9
enrbaewebdaniel <enrbaewebdaniel@gmail.com>
2025-03-08 13:45:35 +0100

    feat: add About and Contact components; update styles and navigation links for improved structure and accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '@app/menu/menu.component';
 import { ExperienceComponent } from '@app/experience/experience.component';
-import { ServicesComponent } from './services/services.component';
+import { ServicesComponent } from '@app/services/services.component';
+import { ContactComponent } from '@app/contact/contact.component';
+import { AboutComponent } from '@app/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, ExperienceComponent, ServicesComponent],
+  imports: [
+    MenuComponent,
+    ExperienceComponent,
+    ServicesComponent,
+    ContactComponent,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
